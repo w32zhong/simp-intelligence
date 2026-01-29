bce816dbed20a1c9556cb3e0fd0317994ad48046
w32zhong <clock126@126.com>
2026-01-28 21:49:46 -0500

    remove tract.

--- simp_intelligence/math/tract/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Test suite for the Tract library.
-
-This module contains comprehensive tests for morphisms, layouts, and their
-interactions in the Tract library.
-"""
-
-# Pytest will automatically discover tests, so we don't need to import anything
-# This file can be empty or just contain documentation
